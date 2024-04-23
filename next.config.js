464d8ccccb0d2e26369d4f74ea09994fde2166d8
Jeff Hamel <jshamel@gmail.com>
2024-04-23 01:45:27 -0400

    misc

--- next.config.js
+++ next.config.js
@@ -1,16 +1,23 @@
 /** @type {import('next').NextConfig} */
-const webpack = require('webpack')
-const nextConfig = {}
+const webpack = require('webpack');
+const nextConfig = {};
 
-const { parsed: myEnv } = require('dotenv').config({
-    path:'.env'
-})
-
-console.log(myEnv);
-
-module.exports = {
-    webpack(config) {
-        config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
-        return config
-    }
+// Load dotenv config for local development
+if (process.env.NODE_ENV !== 'production') {
+  const { parsed: myEnv } = require('dotenv').config({
+    path: '.env',
+  });
+  console.log(myEnv);
+  nextConfig.webpack = function (config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    return config;
+  };
+} else {
+  // Use AWS Amplify environment variables in production
+  nextConfig.webpack = function (config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+    return config;
+  };
 }
+
+module.exports = nextConfig;
